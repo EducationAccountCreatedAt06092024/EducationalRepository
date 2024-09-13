e1b2d914dc2d7afa4a837f4ba95306586f77268d
garrip91 <garrip91@gmail.com>
2024-09-13 14:30:50 +0300

    Fix error v1

--- tasks/task_11.py
+++ tasks/task_11.py
@@ -38,25 +38,3 @@ class Dessert:
     @property
     def is_delicious(self) -> bool:
         return True
-
-
-class JellyBean(Dessert):
-    
-    def __init__(self, name: str | None = None, calories: float | None = None, flavor: str | None = None):
-        super().__init__(name, calories)
-        self._flavor = flavor
-
-    @property
-    def flavor(self) -> str | None:
-        return self._flavor
-
-    @flavor.setter
-    def flavor(self, value) -> None:
-        if isinstance(value, str) or value is None:
-            self._flavor = value
-        else:
-            raise ValueError("flavor must be str | None")
-
-    @property
-    def is_delicious(self) -> bool:
-        return self.flavor is None or self.flavor.lower() != "black licorice"
